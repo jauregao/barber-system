e41cf8c63fbccefd714b634919a9dd9edfd50f9b
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-05-13 18:58:08 -0300

    model de experts

---
+++ prisma/migrations/20240513215447_initial/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "experts" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
+    "avatarUrl" TEXT,
+    "socialLinks" TEXT[],
+
+    CONSTRAINT "experts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "experts_email_key" ON "experts"("email");
