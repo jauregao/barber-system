93674cddc38f64c0aff2b9156aced19539513c0f
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-05-13 17:51:33 -0300

    config: prisma orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
-/* eslint-disable prettier/prettier */
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { Module } from '@nestjs/common'
+import { PrismaModule } from './database/prisma.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-  exports: [],
+	imports: [PrismaModule],
+	controllers: [],
+	providers: []
 })
 export class AppModule {}
