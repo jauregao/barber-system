cfaa5e14ae28225e2c707d20b07ecd723ebb4e99
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-05-29 17:40:41 -0300

    finished adding a customer to the queue

--- src/queuecustomers/queuecustomers.controller.ts
+++ src/queuecustomers/queuecustomers.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common'
+import {
+	Body,
+	Controller,
+	HttpStatus,
+	NotFoundException,
+	Post,
+	Res
+} from '@nestjs/common'
 import { QueuecustomersService } from './queuecustomers.service'
+import { Response } from 'express'
+import CreateCustomerQueueDto from './dtos/create-queueCustomer'
 
 @Controller('queuecustomers')
 export class QueuecustomersController {
 	constructor(private readonly queuecustomersService: QueuecustomersService) {}
+
+	@Post()
+	async addCustomer(
+		@Body() data: CreateCustomerQueueDto,
+		@Res() res: Response
+	) {
+		const queueExists = await this.queuecustomersService.getExpertQueueToday(
+			data.expertId
+		)
+		if (!queueExists) {
+			throw new NotFoundException('Fila não existe.')
+		}
+
+		const customer = await this.queuecustomersService.addCustomer({
+			name: data.name,
+			service: data.service,
+			queueId: queueExists.id
+		})
+		return res.status(HttpStatus.CREATED).json(customer)
+	}
 }
--- src/queuecustomers/queuecustomers.service.ts
+++ src/queuecustomers/queuecustomers.service.ts
@@ -1,20 +1,18 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/database/prisma.service'
-import CreateCustomerQueueDto from './dtos/create-queueCustomer'
+
+type CreateCustomer = {
+	name: string
+	service: string
+	queueId: string
+}
 
 @Injectable()
 export class QueuecustomersService {
 	constructor(private readonly prisma: PrismaService) {}
 
-	async addCustomer(data: CreateCustomerQueueDto) {
-		const queue = await this.getExpertQueueToday(data.expertId)
-		return await this.prisma.queueCustomer.create({
-			data: {
-				name: data.name,
-				service: data.service,
-				queueId: queue.id
-			}
-		})
+	async addCustomer(data: CreateCustomer) {
+		return await this.prisma.queueCustomer.create({ data })
 	}
 
 	async getExpertQueueToday(expertId: string) {
