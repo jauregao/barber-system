5ccbeb83207c461b404a27cdbe52201d69a06c20
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-05-31 19:55:21 -0300

    created login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common'
+import {
+	Controller,
+	HttpStatus,
+	Post,
+	Req,
+	Res,
+	UseGuards
+} from '@nestjs/common'
 import { AuthService } from './auth.service'
+import { Request, Response } from 'express'
+import { AuthGuard } from '@nestjs/passport'
+import { LocalAuthGuard } from './local-auth-guard'
 
-@Controller('auth')
+@Controller()
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
+
+	@UseGuards(LocalAuthGuard)
+	@Post('login')
+	async login(@Req() req: Request, @Res() res: Response) {
+		const { user } = req
+		return res.status(HttpStatus.OK).json(user)
+	}
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { UsersService } from 'src/users/users.service'
+import { LocalStrategy } from './local.strategy'
+import { PassportModule } from '@nestjs/passport'
 
 @Module({
+	imports: [PassportModule],
 	controllers: [AuthController],
-	providers: [AuthService, UsersService]
+	providers: [AuthService, UsersService, LocalStrategy]
 })
 export class AuthModule {}
